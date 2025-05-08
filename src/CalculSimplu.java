038486d9642c1c0b48da54033f837c92371ece99
Dorin2025 <dorin_andrei2000@yahoo.com>
2025-05-08 12:17:08 +0300

    Update CalculSimplu.java

--- src/CalculSimplu.java
+++ src/CalculSimplu.java
@@ -2,6 +2,8 @@ public class CalculSimplu {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("Suma: " + (a+b));
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Diferenta: " + (a - b));
+        System.out.println("Produsul: " + (a * b));
     }
 }
